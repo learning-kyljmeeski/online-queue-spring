a6039e01651f6fccdc19fb716ed8247257beb3fe
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-14 16:20:37 +0600

    feat: implement UserDetailsService

---
+++ src/main/java/com/kyljmeeski/onlinequeue/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kyljmeeski.onlinequeue.service.impl;
+
+import com.kyljmeeski.onlinequeue.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+
+    public UserDetailsServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username).orElseThrow();
+    }
+}
