34aab6984a985f5776332e888a3e084d91adb5a0
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-19 11:55:04 +0600

    feat: redirect if authentication failed

--- src/main/java/com/kyljmeeski/onlinequeue/exception/advice/AuthControllerAdvice.java
+++ src/main/java/com/kyljmeeski/onlinequeue/exception/advice/AuthControllerAdvice.java
@@ -3,6 +3,7 @@ package com.kyljmeeski.onlinequeue.exception.advice;
 import com.kyljmeeski.onlinequeue.exception.UsernameIsTakenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,5 +12,10 @@ public class AuthControllerAdvice {
     @ExceptionHandler(UsernameIsTakenException.class)
     public ResponseEntity<Object> handlePropertyValueException(UsernameIsTakenException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(AuthenticationServiceException.class)
+    public ResponseEntity<Object> handleAuthenticationServiceException(AuthenticationServiceException exception) {
+        return new ResponseEntity<>("/login", HttpStatus.TEMPORARY_REDIRECT);
     }
 }
